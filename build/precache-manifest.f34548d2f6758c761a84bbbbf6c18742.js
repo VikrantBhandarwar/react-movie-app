a330f1166595524506a5748a9bd52b2b24ec6946
Vikrant Bhandarwar <72235685+VikrantBhandarwar@users.noreply.github.com>
2021-02-12 17:21:55 +1100

    build commit

---
+++ build/precache-manifest.f34548d2f6758c761a84bbbbf6c18742.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69e112f0144cdb3b939d89fd628f5a6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a7f34b749daaecfbcd1",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "47f6a295ca6515876410",
+    "url": "/static/js/2.168cfbc2.chunk.js"
+  },
+  {
+    "revision": "10816ad15893520f3fedffc48f8d2203",
+    "url": "/static/js/2.168cfbc2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a7f34b749daaecfbcd1",
+    "url": "/static/js/main.1a718fa8.chunk.js"
+  },
+  {
+    "revision": "7fec9bf4c89f8f9d38a0",
+    "url": "/static/js/runtime-main.992afc9b.js"
+  },
+  {
+    "revision": "4731eafe4920083d95879f06346dec18",
+    "url": "/static/media/404.4731eafe.svg"
+  },
+  {
+    "revision": "bca418f347005a3e244cb65b8c24c2c0",
+    "url": "/static/media/abstract_movie_poster.bca418f3.svg"
+  }
+]);
